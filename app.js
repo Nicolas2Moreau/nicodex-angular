35885f7cd1555938b153eabb8cd7cc500b85f91e
nicolas2moreau <nicolas2moreau@gmail.com>
2023-10-02 15:51:04 +0200

    feat(video service) working electron web TO desktop app + media player microservice integration

--- app.js
+++ app.js
@@ -2,19 +2,43 @@ const {
     app,
     BrowserWindow
 } = require('electron')
-
+const path = require('path')
 let appWindow
-
+let appPath
 function createWindow() {
     appWindow = new BrowserWindow({
         width: 1000,
-        height: 800
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+          },
+          options:{
+			fullscreen:true
+		}
     })
-    appWindow.loadFile('dist/nicodex-app/index.html')
+    appWindow.loadFile('./dist/nicodex-app/index.html')
+    // appWindow.loadURL(url.format({
+    //     pathname: path.join(__dirname, './dist/nicodex-app/index.html'),
+    //     protocol: 'file:',
+    //     slashes: true
+    //   }))
     appWindow.on('closed', function(){
         appWindow = null
     })
+    // appWindow.webContents. 
+    appWindow.setMenu(null);
+
+  
+    appWindow.setFullScreen(true);
+    appWindow.maximize();
 }
 app.whenReady().then(()=> {
-    createWindow()
+    appPath=app.getAppPath();
+    console.log(appPath);
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+          createWindow()
+        }
+      })
 })
